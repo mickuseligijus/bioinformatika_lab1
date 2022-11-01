29733e94750213eaf5c3297a85d868dc049a778e
Eligijus Mickus <mickus.eligijus@gmail.com>
2022-11-01 18:47:27 +0200

    removing code

--- lab1.py
+++ lab1.py
@@ -157,57 +157,6 @@ filtered_f6 = get_filtered_pairs(pairs_f6)
 codons = get_codons()
 dicodons = get_dicodons(codons)
 
-#finds frequency of codons in a specific reading frame
-# def get_codons_frequency(pairs, sequence_frame, codons):
-#     list = []
-#     for p in pairs:
-#         (a,b) = p
-#         l = sequence_frame[a:b+1]
-#         list.append(l)
-    
-#     f_list = []
-#     for l in list:
-#         acc = []
-#         for codon in codons:
-#             index = 0
-#             for element in l:
-#                 if element == codon:
-#                     index=index+1
-#             acc.append((codon,index))
-#         f_list.append(acc)
-
-#     accumulator = []
-#     for codon in codons:
-#         accumulator.append((codon,0))
-
-#     for l in f_list:
-#         for pair in l:
-#             (codone,frequency) = pair
-#             for i in range(len(accumulator)):
-#                 (codone_acc, frequency_acc) = accumulator[i]
-#                 if codone_acc == codone:
-#                     frequency_acc+=frequency
-#                     accumulator[i] = (codone,frequency_acc)
-
-#     return accumulator
-
-    # print(len(pairs))
-                
-#######################################################################################
-
-# frequency1 = get_codons_frequency(filtered_f1,sequence_frame1,codons)
-# frequency2 = get_codons_frequency(filtered_f2,sequence_frame2,codons)
-# frequency3 = get_codons_frequency(filtered_f3,sequence_frame3,codons)
-
-# frequency4 = get_codons_frequency(filtered_f4,reverse_sequence_frame1,codons)
-# frequency5 = get_codons_frequency(filtered_f5,reverse_sequence_frame2,codons)
-# frequency6 = get_codons_frequency(filtered_f6,reverse_sequence_frame3,codons)
-
-# frenquencies = [frequency1,frequency2,frequency3,frequency4,frequency5,frequency6]
-# print(frenquencies)
-
-
-
 def get_dicodons_sequences(filtered_pairs, sequence_frame):
     list = []
     for p in filtered_pairs:
@@ -230,7 +179,15 @@ def get_dicodons_sequences(filtered_pairs, sequence_frame):
     
     return dicodons_list_frame
 
-# print(get_dicodons_sequences(filtered_f1, sequence_frame1))
+    
+def get_codons_sequence(filtered_pairs,sequence_frame):
+    acc = []
+    for p in filtered_pairs:
+        (a,b) = p
+        l = sequence_frame[a:b+1]
+        acc = concat_list(acc,l)
+
+    return acc
 
 def get_frequency(pattern, filtered_frame_list, get_sequence):
     acc = []
@@ -250,51 +207,6 @@ def get_frequency(pattern, filtered_frame_list, get_sequence):
     
     return frenquencies
 
-# def get_dicodons_frequency(filtered_frame_list):
-
-#     acc_dicodons = []
-#     for filtered_frame in filtered_frame_list:
-#         (filtered, frame) = filtered_frame
-#         dicodons_sequence = get_dicodons_sequences(filtered,frame)
-#         acc_dicodons = concat_list(acc_dicodons, dicodons_sequence)
-
-#     frenquencies = []
-#     for dicodon in dicodons:
-#         counter = 0
-#         for dicodon_sequence in acc_dicodons:
-#             if dicodon == dicodon_sequence:
-#                 counter = counter +1
-#         frenquencies.append(dicodon,counter)
-    
-#     return frenquencies
-
-# def get_codons_frequency(filtered_frame_list,a):
-
-#     acc_codons = []
-#     for filtered_frame in filtered_frame_list:
-#         (filtered, frame) = filtered_frame
-#         codons_sequence = get_codons_sequence(filtered,frame)
-#         acc_dicodons = concat_list(acc_dicodons, codons_sequence)
-
-#     frenquencies = []
-#     for codon in codons:
-#         counter = 0
-#         for codon_sequence in acc_codons:
-#             if codon == codon_sequence:
-#                 counter = counter +1
-#         frenquencies.append(codon,counter)
-    
-#     return frenquencies
-    
-def get_codons_sequence(filtered_pairs,sequence_frame):
-    acc = []
-    for p in filtered_pairs:
-        (a,b) = p
-        l = sequence_frame[a:b+1]
-        acc = concat_list(acc,l)
-
-    return acc
-
 def concat_list(list1, list2):
     for i in list2 :
         list1.append(i)
@@ -308,83 +220,9 @@ filtered_frame_list = [
     (filtered_f5,reverse_sequence_frame2),
     (filtered_f6,reverse_sequence_frame3)]
 
-# get_dicodons_frequency(filtered_frame_list)
-print(get_frequency(codons,filtered_frame_list,get_codons_sequence))
-
-
-#sums up frequency of different reading frames
-# def sum_up_frequency(f_list):
-#     accumulator = []
-#     for codon in codons:
-#         accumulator.append((codon,0))
-
-#     for l in f_list:
-#         for pair in l:
-#             (codone,frequency) = pair
-#             for i in range(len(accumulator)):
-#                 (codone_acc, frequency_acc) = accumulator[i]
-#                 if codone_acc == codone:
-#                     frequency_acc+=frequency
-#                     accumulator[i] = (codone,frequency_acc)
-
-#     return accumulator
-
-# print(sum_up_frequency(frenquencies))
-
+####################################### 4 dalis kodonu ir dikodonu dazniai #############
 codons_frequency = get_frequency(codons,filtered_frame_list,get_codons_sequence)
 dicodons_frequency = get_frequency(dicodons,filtered_frame_list,get_dicodons_sequences)
-
-print(dicodons_frequency)
-# v2 = sum_up_frequency(frenquencies)
-
-# print(v1==v2)
+########################################################################################
 
 
-
-
-
-
-
-
-
-
-# codons = {
-#     "TTT","TTC","TTA","TTG",
-#     "TCT","TCC","TCA","TCG",
-#     "TAT","TAC","TAA","TAG",
-#     "TGT","TGC","TGA","TGG",
-
-#     "CTT","CTC","CTA","CTG",
-#     "CCT","CCC","CCA","CCG",
-#     "CAT","CAC","CAA","CAG",
-#     "CGT","CGC","CGA","CGG",
-
-#     "ATT","ATC","ATA","ATG",
-#     "ACT","ACC","ACA","ACG",
-#     "AAT","AAC","AAA","AAG",
-#     "AGT","AGC","AGA","AGG",
-
-#     "GTT","GTC","GTA","GTG",
-#     "GCT","GCC","GCA","GCG",
-#     "GAT","GAC","GAA","GAG",
-#     "GGT","GGC","GGA","GGA"
-#     }
-
-# x = "AAAATGTACTATTAG"
-
-# p = get_reading_frame(x)
-# m = get_pairs(p)
-# print(m)
-
-# t = get_filtered_pairs(m)
-
-
-
-# print("pirmas: \n") 
-# print(listFrame1)
-# print("antras: \n") 
-# print(listFrame2)
-# print("trecias: \n") 
-# print(listFrame3)
-
-# print(reverse("ACGTTTGA"))
