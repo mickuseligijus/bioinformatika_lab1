d0aac7c2cc3de9bebba8fbe23a5ee5fa752b4727
Eligijus Mickus <mickus.eligijus@gmail.com>
2022-11-03 14:31:25 +0200

    adding distribution, distance calculation

--- lab1.py
+++ lab1.py
@@ -113,45 +113,45 @@ ending1 = "TAA"
 ending2 = "TGA"
 ending3 = "TAG"
 
-reverse_sequence = reverse(sequence)
+# reverse_sequence = reverse(sequence)
 
-sequence_frame1 = get_reading_frame(sequence)
-sequence_frame2 = get_reading_frame(sequence[1:])
-sequence_frame3 = get_reading_frame(sequence[2:])
+# sequence_frame1 = get_reading_frame(sequence)
+# sequence_frame2 = get_reading_frame(sequence[1:])
+# sequence_frame3 = get_reading_frame(sequence[2:])
 
-reverse_sequence_frame1 = get_reading_frame(reverse_sequence)
-reverse_sequence_frame2 = get_reading_frame(reverse_sequence[1:])
-reverse_sequence_frame3 = get_reading_frame(reverse_sequence[2:])
+# reverse_sequence_frame1 = get_reading_frame(reverse_sequence)
+# reverse_sequence_frame2 = get_reading_frame(reverse_sequence[1:])
+# reverse_sequence_frame3 = get_reading_frame(reverse_sequence[2:])
 
 
 ####################################### 1 dalis POROS #################################
-pairs_f1 = get_pairs(sequence_frame1)
-pairs_f2 = get_pairs(sequence_frame2)
-pairs_f3 = get_pairs(sequence_frame3)
+# pairs_f1 = get_pairs(sequence_frame1)
+# pairs_f2 = get_pairs(sequence_frame2)
+# pairs_f3 = get_pairs(sequence_frame3)
 
-pairs_f4 = get_pairs(reverse_sequence_frame1)
-pairs_f5 = get_pairs(reverse_sequence_frame2)
-pairs_f6 = get_pairs(reverse_sequence_frame3)
+# pairs_f4 = get_pairs(reverse_sequence_frame1)
+# pairs_f5 = get_pairs(reverse_sequence_frame2)
+# pairs_f6 = get_pairs(reverse_sequence_frame3)
 #######################################################################################
 
 ####################################### 2 dalis tolimiausias STOP START ###############
-pairs_stop_f1 = get_furthest_start(sequence_frame1)
-pairs_stop_f2 = get_furthest_start(sequence_frame2)
-pairs_stop_f3 = get_furthest_start(sequence_frame3)
+# pairs_stop_f1 = get_furthest_start(sequence_frame1)
+# pairs_stop_f2 = get_furthest_start(sequence_frame2)
+# pairs_stop_f3 = get_furthest_start(sequence_frame3)
 
-pairs_stop_f4 = get_furthest_start(reverse_sequence_frame1)
-pairs_stop_f5 = get_furthest_start(reverse_sequence_frame2)
-pairs_stop_f6 = get_furthest_start(reverse_sequence_frame3)
+# pairs_stop_f4 = get_furthest_start(reverse_sequence_frame1)
+# pairs_stop_f5 = get_furthest_start(reverse_sequence_frame2)
+# pairs_stop_f6 = get_furthest_start(reverse_sequence_frame3)
 #######################################################################################
 
 ####################################### 3 dalis fragmentai > 100 ######################
-filtered_f1 = get_filtered_pairs(pairs_f1)
-filtered_f2 = get_filtered_pairs(pairs_f2)
-filtered_f3 = get_filtered_pairs(pairs_f3)
+# filtered_f1 = get_filtered_pairs(pairs_f1)
+# filtered_f2 = get_filtered_pairs(pairs_f2)
+# filtered_f3 = get_filtered_pairs(pairs_f3)
 
-filtered_f4 = get_filtered_pairs(pairs_f4)
-filtered_f5 = get_filtered_pairs(pairs_f5)
-filtered_f6 = get_filtered_pairs(pairs_f6)
+# filtered_f4 = get_filtered_pairs(pairs_f4)
+# filtered_f5 = get_filtered_pairs(pairs_f5)
+# filtered_f6 = get_filtered_pairs(pairs_f6)
 #######################################################################################
 
 codons = get_codons()
@@ -212,17 +212,169 @@ def concat_list(list1, list2):
         list1.append(i)
     return list1
 
-filtered_frame_list = [
-    (filtered_f1,sequence_frame1),
-    (filtered_f2,sequence_frame2),
-    (filtered_f3,sequence_frame3),
-    (filtered_f4,reverse_sequence_frame1),
-    (filtered_f5,reverse_sequence_frame2),
-    (filtered_f6,reverse_sequence_frame3)]
+# filtered_frame_list = [
+#     (filtered_f1,sequence_frame1),
+#     (filtered_f2,sequence_frame2),
+#     (filtered_f3,sequence_frame3),
+#     (filtered_f4,reverse_sequence_frame1),
+#     (filtered_f5,reverse_sequence_frame2),
+#     (filtered_f6,reverse_sequence_frame3)]
 
 ####################################### 4 dalis kodonu ir dikodonu dazniai #############
-codons_frequency = get_frequency(codons,filtered_frame_list,get_codons_sequence)
-dicodons_frequency = get_frequency(dicodons,filtered_frame_list,get_dicodons_sequences)
+# codons_frequency1 = get_frequency(codons,filtered_frame_list,get_codons_sequence)
+# dicodons_frequency1 = get_frequency(dicodons,filtered_frame_list,get_dicodons_sequences)
 ########################################################################################
+
+
+
+def combine_methods(seq):
+
+    reverse_seq = reverse(seq)
+    filtered_reading_frames = concat_list(get_shifting(seq), get_shifting(reverse_seq))
+
+    return filtered_reading_frames
+
+
+def get_shifting(seq):
+    reading_frames = []
+    for i in range(3):
+        frame = get_reading_frame(seq[i:])
+        pairs = get_pairs(frame)
+        filtered_pairs = get_filtered_pairs(pairs)
+        reading_frames.append((filtered_pairs,frame))
+    
+    return reading_frames
+
+def calculate_distance(seq_frequency):
+
+    matrixCodons = []
+    matrixDicodons = []
+    sum = 0
+    for s1 in seq_frequency:
+        (info,codon1,dicodon1) = s1
+        row = []
+        rowDicodons = []
+        for s2 in seq_frequency:
+            (info2,codon2,dicodon2) = s2
+            sum = 0
+            sumDicodons = 0
+            for i in range(len(codon1)):
+                (name1,fr1) = codon1[i]
+                (name2,fr2) = codon2[i]
+
+                distance = (abs(fr1-fr2))
+                sum = sum + distance
+
+            for ii in range(len(dicodon1)):
+                (name3,fr3) = dicodon1[ii]
+                (name4,fr4) = dicodon2[ii]
+
+                distanceDicodons = (abs(fr3-fr4))
+                sumDicodons = sumDicodons + distanceDicodons
+
+            row.append(sum**1/2)
+            rowDicodons.append(sumDicodons**1/2)
+
+        matrixCodons.append((info,row))
+        matrixDicodons.append((info,rowDicodons))
+    
+    return (matrixCodons,matrixDicodons)
+
+
+
+def print_matrix(matrix):
+    print(len(matrix))
+    for row in matrix:
+        (info, values) = row
+        print(info, end = ' ')
+        for v in values:
+            print(v, end = ' ')
+        print()
+
+def print_distribution(seq_fr):
+    print("\n")
+    most_common(seq_fr)
+    print("\n")
+    for s in seq_fr:
+        (info, codons, dicodons) = s
+        co = sorted(codons, key=lambda tup: tup[1], reverse=True)
+        dico =sorted(dicodons, key=lambda tup: tup[1], reverse=True)
+        print(info, co[0], co[1], co[2])
+        print(info, dico[0], dico[1], dico[2])
+    
+
+def most_common(list):
+    codon_list = []
+    dicodon_list = []
+
+    for codon in codons:
+        accCodon = 0
+        for l in list:
+            (_, co, _) = l
+            for c in co:
+                (name, amountCodon) = c
+                if name == codon:
+                    accCodon += amountCodon
+        codon_list.append((codon,accCodon))
+
+    for dicodon in dicodons:
+        accDicodon = 0
+        for l in list:
+            (_, _, dico) = l
+            for d in dico:
+                (name, amountDicoodon) = d
+                if name == dicodon:
+                    accDicodon += amountDicoodon
+        dicodon_list.append((dicodon,accDicodon))
+
+    co = sorted(codon_list, key=lambda tup: tup[1], reverse=True)
+    dico =sorted(dicodon_list, key=lambda tup: tup[1], reverse=True)
+
+    print("codons")
+    print(co[0], co[1], co[2])
+    print("dicodons")
+    print(dico[0], dico[1], dico[2])
+
+
+
+        
+# def max_number(x):
+#     temp = 0
+#     code = ""
+#     previous =""
+#     temp_previous = 0
+#     for i in x:
+#         (a,b) = i
+#         if b>=temp:
+#             temp_previous = temp
+#             previous = code
+#             if temp == 0:
+#                 previous = a
+#                 temp_previous = b
+#             temp = b  
+#             code = a
+
+    
+#     return ((code, temp), (previous,temp_previous))
+
+data_sources = ["data/bacterial1.fasta","data/bacterial2.fasta","data/bacterial3.fasta","data/bacterial4.fasta","data/mamalian1.fasta","data/mamalian2.fasta","data/mamalian3.fasta","data/mamalian4.fasta"]
+
+seq_frequency = []
+for location in data_sources:
+    info = location.replace("data/", "")
+    info = info.replace(".fasta", "")
+    (meta_info, seq) = read_from_file(location)
+    filtered_reading_frames = combine_methods(seq)
+    codons_frequency = get_frequency(codons,filtered_reading_frames,get_codons_sequence)
+    dicodons_frequency = get_frequency(dicodons,filtered_reading_frames,get_dicodons_sequences)
+    seq_frequency.append((info,codons_frequency,dicodons_frequency))        
+
+(matrix_codons, matrix_dicodons) = calculate_distance(seq_frequency)
+print_matrix(matrix_codons)
+print_matrix(matrix_dicodons)
+print_distribution(seq_frequency)
+
+
+
 
 
